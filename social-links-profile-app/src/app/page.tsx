a007402fbf5ae6f47a7a5f40c880f1ae736587db
Chelsea Cruz <125297600+chelsea-here@users.noreply.github.com>
2024-07-05 22:27:47 -0500

    updated html references

--- social-links-profile-app/src/app/page.tsx
+++ social-links-profile-app/src/app/page.tsx
@@ -3,11 +3,8 @@ import avatar from "/public/avatar-jessica.jpeg"
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center bg-off-black w-screen h-screen px-6">
-      {/* main elements centers everything horizontally */}
-      <div className="flex flex-col justify-center items-center w-full max-w-[24rem] h-full max-h-[38.1875rem] bg-dark-gray rounded-xl mx-auto">
-        {/* The first div justifies containing items vertically. note: this does not center the attribution text, 
-             so the screenshot will align with example. */}
+    <body className="flex flex-col justify-center items-center bg-off-black w-screen h-screen px-6">
+      <main className="flex flex-col justify-center items-center w-full max-w-[24rem] h-full max-h-[38.1875rem] bg-dark-gray rounded-xl mx-auto">
         <div className="flex flex-col w-full max-w-[19rem] items-center gap-6 m-auto">
           <Image
             className="w-[5.5rem] h-auto rounded-full aspect-square object-cover"
@@ -30,15 +27,13 @@ export default function Home() {
             <a className="hover:bg-green hover:text-gray hover:cursor-pointer focus:bg-green focus:text-gray text-white text-sm font-bold w-full p-3 bg-gray rounded-lg leading-normal" href="https://www.instagram.com">Instagram</a>
           </div>
         </div>
-      </div>
-      <div className="mt-2 left-0 right-0 justify-self-center">
-        <footer id="attribution">
-          <p className="text-white text-center text-[0.6875rem]">Challenge by <a className="text-blue" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-          </p>
-          <p className="text-white text-center text-[0.6875rem]">Coded by <a className="text-blue" href="#">Chelsea Anne Livingston Cruz</a>.
-          </p>
-        </footer>
-      </div>
-    </main>
+      </main>
+      <footer id="attribution" className="mt-2 left-0 right-0 justify-self-center">
+        <p className="text-white text-center text-[0.6875rem]">Challenge by <a className="text-blue" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        </p>
+        <p className="text-white text-center text-[0.6875rem]">Coded by <a className="text-blue" href="#">Chelsea Anne Livingston Cruz</a>.
+        </p>
+      </footer>
+    </body>
   );
 }
